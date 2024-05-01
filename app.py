944fdf718ff3ec14ff7b996025de0e57e26b7a71
RenaudLN <renaudlaine31@gmail.com>
2024-05-01 20:41:57 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,96 @@
+import dash_mantine_components as dmc
+from dash import Dash, Input, Output, callback
+from transferlist_aio import TransferList
+from dash_iconify import DashIconify
+
+app = Dash(
+    __name__,
+    external_stylesheets = [
+        "https://unpkg.com/@mantine/dates@7/styles.css",
+        "https://unpkg.com/@mantine/code-highlight@7/styles.css",
+        "https://unpkg.com/@mantine/charts@7/styles.css",
+        "https://unpkg.com/@mantine/carousel@7/styles.css",
+        "https://unpkg.com/@mantine/notifications@7/styles.css",
+        "https://unpkg.com/@mantine/nprogress@7/styles.css",
+    ]
+)
+
+initial_values = [
+    [
+        {"value": "react", "label": "React"},
+        {"value": "ng", "label": "Angular"},
+        {"value": "next", "label": "Next.js"},
+        {"value": "blitz", "label": "Blitz.js"},
+        {"value": "gatsby", "label": "Gatsby.js"},
+        {"value": "vue", "label": "Vue"},
+        {"value": "jq", "label": "jQuery"},
+    ],
+    [
+        {"value": "sv", "label": "Svelte"},
+        {"value": "rw", "label": "Redwood"},
+        {"value": "np", "label": "NumPy"},
+        {"value": "dj", "label": "Django"},
+        {"value": "fl", "label": "Flask"},
+    ],
+]
+
+app.layout = dmc.MantineProvider(
+    dmc.Container(
+        [
+            dmc.Title("DMC 0.14 TransferList", mb=32),
+            TransferList(
+                aio_id="transferlist",
+                value=initial_values,
+                breakpoint="sm",
+                # limit=5,
+                listHeight=200,
+                nothingFound="Nothing matches your search",
+                placeholder="No items",
+                radius="sm",
+                searchPlaceholder="Search...",
+                showTransferAll=True,
+                titles=["Source", "Destination"],
+                transferAllMatchingFilters=True,
+            ),
+            dmc.Text(id="display", py="1rem"),
+        ]
+    )
+)
+
+
+@callback(
+    Output("display", "children"),
+    Input(TransferList.ids.main("transferlist"), "value"),
+)
+def udpate_display(values):
+    return dmc.SimpleGrid(
+        [
+            dmc.Stack(
+                [
+                    dmc.Text("Items in source:", fw=600),
+                    dmc.List(
+                        [
+                            dmc.ListItem(f'{v["label"]} ({v["value"]})')
+                            for v in values[0]
+                        ]
+                    )
+                ],
+            ),
+            dmc.Stack(
+                [
+                    dmc.Text("Items in destination:", fw=600),
+                    dmc.List(
+                        [
+                            dmc.ListItem(f'{v["label"]} ({v["value"]})')
+                            for v in values[1]
+                        ]
+                    )
+                ],
+            ),
+        ],
+        cols=2,
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ transferlist_aio.py
@@ -0,0 +1,319 @@
+import json
+from functools import partial
+from typing import Literal
+
+import dash_mantine_components as dmc
+from dash import ALL, MATCH, Input, Output, State, callback, clientside_callback, ctx, no_update
+from dash_iconify import DashIconify
+
+
+def base_id(part: str, aio_id: str):
+    return {"part": part, "aio_id": aio_id}
+
+
+def side_id(part: str, aio_id: str, side: Literal["left", "right"]):
+    return {"part": part, "aio_id": aio_id, "side": side}
+
+
+class TransferList(dmc.SimpleGrid):
+    """TransferList AIO component to get the DMC 0.12 working with 0.14
+
+    :param aio_id: id of the AIO component
+    :param breakpoint: Mantine breakpoint value, shifting from row to column layout
+    :param limit: limit the number of items in the checklist
+    :param listHeight: height of the checklist
+    :param nothingFound: text to display when nothing matches the search
+    :param placeholder: text to display when the checklist is empty
+    :param radius: Mantine border radius for the lists
+    :param searchPlaceholder: placeholder for the search input
+    :param showTransferAll: show the transfer all button
+    :param titles: titles of the lists
+    :param transferAllMatchingFilters: Whether to transfer all or all matching filters
+    :param value: initial value of the lists
+    """
+
+    class ids:
+        search = partial(side_id, "__trl-search-input")
+        transfer = partial(side_id, "__trl-transfer-input")
+        transfer_all = partial(side_id, "__trl-transfer-all-input")
+        checklist = partial(side_id, "__trl-checklist-input")
+        main = partial(base_id, "__trl-main")
+
+    def __init__(
+        self,
+        aio_id: str,
+        breakpoint: str = None,
+        limit: int = None,
+        listHeight: int = 150,
+        nothingFound: str = None,
+        placeholder: str = None,
+        radius: str = "sm",
+        searchPlaceholder: str = None,
+        showTransferAll: bool = True,
+        titles: list[str] = None,
+        transferAllMatchingFilters: bool = True,
+        value: list = None,
+        **kwargs
+    ):
+        super().__init__(
+            [
+                dmc.Stack(
+                    [
+                        *([dmc.Text(titles[0], fw=600)] if titles else []),
+                        dmc.Paper(
+                            [
+                                dmc.Group(
+                                    [
+                                        self.search_input(aio_id, "left", placeholder=searchPlaceholder),
+                                        self.transfer(aio_id, "left", showTransferAll),
+                                        *([self.transfer_all(aio_id, "left")] if showTransferAll else []),
+                                    ],
+                                    style={"alignItems": "initial"},
+                                    gap=0,
+                                ),
+                                self.checklist(aio_id, "left", value[0], listHeight, limit),
+                            ],
+                            radius=radius,
+                            withBorder=True,
+                            style={"overflow": "hidden"},
+                        ),
+                    ],
+                    gap="0.375rem",
+                ),
+                dmc.Stack(
+                    [
+                        *([dmc.Text(titles[1], fw=600)] if titles else []),
+                        dmc.Paper(
+                            [
+                                dmc.Group(
+                                    [
+                                        *([self.transfer_all(aio_id, "right")] if showTransferAll else []),
+                                        self.transfer(aio_id, "right", showTransferAll),
+                                        self.search_input(aio_id, "right", placeholder=searchPlaceholder),
+                                    ],
+                                    style={"alignItems": "initial"},
+                                    gap=0,
+                                ),
+                                self.checklist(aio_id, "right", value[1], listHeight, limit),
+                            ],
+                            radius=radius,
+                            withBorder=True,
+                            style={"overflow": "hidden"},
+                        ),
+                    ],
+                    gap="0.375rem",
+                ),
+                dmc.JsonInput(
+                    id=self.ids.main(aio_id),
+                    style={"display": "none"},
+                    value=value,
+                    **{
+                        "data-placeholder": json.dumps(placeholder),
+                        "data-nothingfound": json.dumps(nothingFound),
+                        "data-transferallmatchingfilters": json.dumps(transferAllMatchingFilters),
+                    },
+                ),
+            ],
+            cols={"base": 1, breakpoint: 2} if breakpoint else 2,
+            spacing="md",
+            **kwargs
+        )
+
+    @classmethod
+    def checkbox(cls, value: dict):
+        return dmc.Checkbox(
+            label=value["label"],
+            value=value["value"],
+            px="0.25rem",
+            styles={
+                "labelWrapper": {"flex": 1},
+                "label": {"cursor": "pointer", "padding": "0.5rem 0"},
+                "body": {"alignItems": "center", "gap": "0.5rem"},
+            },
+        )
+
+    @classmethod
+    def checklist(
+        cls, aio_id: str, position: Literal["left", "right"], value: list[dict], list_height: int, limit: int = None
+    ):
+        if limit:
+            value = value[:limit]
+        return dmc.ScrollArea(
+            dmc.CheckboxGroup(
+                [cls.checkbox(val) for val in value],
+                py="0.25rem",
+                id=cls.ids.checklist(aio_id, position),
+            ),
+            style={"height": list_height},
+            px="0.5rem",
+        )
+
+    @classmethod
+    def search_input(cls, aio_id: str, position: Literal["left", "right"], **kwargs):
+        return dmc.TextInput(
+            styles={
+                "root": {"flex": 1},
+                "input": {
+                    "borderTop": "none",
+                    "borderLeft": "none",
+                    "borderRight": "none",
+                }
+            },
+            radius=0,
+            id=cls.ids.search(aio_id, position),
+            debounce=250,
+            **kwargs,
+        )
+
+    @classmethod
+    def transfer_all(cls, aio_id: str, position: Literal["left", "right"]):
+        icon_side = "left" if position == "right" else "right"
+        return dmc.Paper(
+            dmc.UnstyledButton(
+                DashIconify(icon=f"uiw:d-arrow-{icon_side}", height=12),
+                style={"height": 34, "width": 34, "display": "grid", "placeContent": "center"},
+                id=cls.ids.transfer_all(aio_id, position)
+            ),
+            withBorder=True,
+            radius=0,
+            style={
+                "borderTop": "none",
+                "borderLeft": "none",
+                "borderRight": "none",
+            }
+        )
+
+    @classmethod
+    def transfer(cls, aio_id: str, position: Literal["left", "right"], show_transfer_all: bool):
+        icon_side = "left" if position == "right" else "right"
+        return dmc.Paper(
+            dmc.UnstyledButton(
+                DashIconify(icon=f"uiw:{icon_side}", height=12),
+                style={"height": 34, "width": 34, "display": "grid", "placeContent": "center"},
+                id=cls.ids.transfer(aio_id, position)
+            ),
+            withBorder=True,
+            radius=0,
+            style={"borderTop": "none"} | (
+                {"borderRight": "none"}
+                if not show_transfer_all and position == "right"
+                else {}
+            ) | (
+                {"borderLeft": "none"}
+                if not show_transfer_all and position == "left"
+                else {}
+            )
+        )
+
+
+@callback(
+    Output(TransferList.ids.checklist(MATCH, MATCH), "children"),
+    Output(TransferList.ids.checklist(MATCH, MATCH), "value"),
+    Input(TransferList.ids.search(MATCH, MATCH), "value"),
+    State(TransferList.ids.main(MATCH), "value"),
+    State(TransferList.ids.main(MATCH), "data-nothingfound"),
+    State(TransferList.ids.main(MATCH), "data-placeholder"),
+    State(TransferList.ids.checklist(MATCH, MATCH), "value"),
+    prevent_initial_call=True,
+)
+def update_checklist(search, values, nothing_found, placeholder,selection):
+    if not ctx.triggered_id:
+        return no_update, no_update
+    value = values[0] if ctx.triggered_id["side"] == "left" else values[1]
+    filtered = [v for v in value if not search or search.lower() in v["label"].lower()]
+    children = None
+    filtered_values = [f["value"] for f in filtered]
+    updated_selection = [s for s in (selection or []) if s in filtered_values]
+    if filtered:
+        children = [TransferList.checkbox(val) for val in filtered]
+    elif search and nothing_found:
+        children = dmc.Text(json.loads(nothing_found), p="0.5rem", c="dimmed")
+    elif not search and placeholder:
+        children = dmc.Text(json.loads(placeholder), p="0.5rem", c="dimmed")
+    return children, updated_selection
+
+
+@callback(
+    Output(TransferList.ids.main(MATCH), "value"),
+    Output(TransferList.ids.checklist(MATCH, ALL), "children", allow_duplicate=True),
+    Output(TransferList.ids.checklist(MATCH, ALL), "value", allow_duplicate=True),
+    Output(TransferList.ids.search(MATCH, ALL), "value", allow_duplicate=True),
+    Input(TransferList.ids.transfer(MATCH, ALL), "n_clicks"),
+    Input(TransferList.ids.transfer_all(MATCH, ALL), "n_clicks"),
+    State(TransferList.ids.checklist(MATCH, ALL), "value"),
+    State(TransferList.ids.search(MATCH, ALL), "value"),
+    State(TransferList.ids.main(MATCH), "value"),
+    State(TransferList.ids.main(MATCH), "data-placeholder"),
+    State(TransferList.ids.main(MATCH), "data-transferallmatchingfilters"),
+    prevent_initial_call=True,
+)
+def transfer(trigger1, trigger2, selection, search, current_value, placeholder, transfer_matching):
+    if not (ctx.triggered_id and (any(trigger1) or any(trigger2))):
+        return no_update, no_update, no_update, no_update
+
+    side = ctx.triggered_id["side"]
+    if ctx.triggered_id["part"] == TransferList.ids.transfer("", "")["part"]:
+        transferred = selection[0] if side == "left" else selection[1]
+    else:
+        if json.loads(transfer_matching):
+            search_ = search[0 if side == "left" else 1]
+            transferred = [
+                v["value"] for v in current_value[0 if side == "left" else 1]
+                if not search_ or search_.lower() in v["label"].lower()
+            ]
+        else:
+            transferred = [v["value"] for v in current_value[0 if side == "left" else 1]]
+
+    if not transferred:
+        return no_update, [no_update] * 2, [no_update] * 2, [no_update] * 2
+
+    if side == "left":
+        new_value = [
+            [v for v in current_value[0] if v["value"] not in transferred],
+            current_value[1] + [v for v in current_value[0] if v["value"] in transferred],
+        ]
+    else:
+        new_value = [
+            current_value[0] + [v for v in current_value[1] if v["value"] in transferred],
+            [v for v in current_value[1] if v["value"] not in transferred],
+        ]
+
+    placeholder = json.loads(placeholder)
+    new_children = [
+        [TransferList.checkbox(v) for v in new_value[0]]
+        if new_value[0]
+        else dmc.Text(placeholder, p="0.5rem", c="dimmed"),
+        [TransferList.checkbox(v) for v in new_value[1]]
+        if new_value[1]
+        else dmc.Text(placeholder, p="0.5rem", c="dimmed"),
+    ]
+    return new_value, new_children, [[]] * 2, [""] * 2
+
+
+clientside_callback(
+    """(selection, style) => {
+        return {
+            ...style,
+            color: !!selection.length ? null : "gray",
+            cursor: !!selection.length ? "pointer" : "default",
+        }
+    }""",
+    Output(TransferList.ids.transfer(MATCH, MATCH), "style"),
+    Input(TransferList.ids.checklist(MATCH, MATCH), "value"),
+    State(TransferList.ids.transfer(MATCH, MATCH), "style"),
+)
+
+
+clientside_callback(
+    """(filtered, style) => {
+        const disabled = !filtered || !filtered.length
+        return {
+            ...style,
+            color: !disabled ? null : "gray",
+            cursor: !disabled ? "pointer" : "default",
+        }
+    }""",
+    Output(TransferList.ids.transfer_all(MATCH, MATCH), "style"),
+    Input(TransferList.ids.checklist(MATCH, MATCH), "children"),
+    State(TransferList.ids.transfer_all(MATCH, MATCH), "style"),
+)
